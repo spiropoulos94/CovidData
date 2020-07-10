923bb5c8921081719b84ce1249b46c8306c71ec2
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-07-10 12:13:57 +0300

    updates

--- app.js
+++ app.js
@@ -1,116 +1,163 @@
+window.onload = function() {
+  //let body = document.getElementById("body");
+
+  let options = document.getElementById("country-select");
+
+  let statsDiv = document.createElement("div");
+  statsDiv.id = "statsDiv";
+
+  statsDiv.style = "display: none";
+  statsDiv.className = "container p-3 mb-2 bg-primary text-white";
+
+  body.appendChild(statsDiv);
+
+  let submitBtn = document.getElementById("submitBtn");
+
+  let selectedCountry = document.getElementById("country-select");
+
+  let allCountriesNames = [];
+
+  // this function takes all the countries names and adds it to be used as the autocomplete values
+
+  function getNames() {
+    fetch("https://api.covid19api.com/summary")
+      .then((response) => response.json())
+      .then((countries) => countries["Countries"])
+      .then((countries) =>
+        countries.forEach((country) => allCountriesNames.push(country))
+      )
+      .then((names) => {
+        for (let i = 0; i < allCountriesNames.length; i++) {
+          options.innerHTML += ` <option value=${allCountriesNames[i]["Slug"]}>${allCountriesNames[i]["Country"]}</option>`;
+        }
+      });
+  }
+
+  getNames();
+
+  //this function retrieves the summary data.
+
+  function getData() {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("https://api.covid19api.com/summary", requestOptions)
+      .then((response) => response.json())
+      .then((response) => response["Countries"])
+      .then((countries) => {
+        //let form = document.getElementById('countryname')
+        let data = selectedCountry.value;
+
+        for (let i = 0; i < countries.length; i++) {
+          if (data.toUpperCase() === countries[i]["Slug"].toUpperCase()) {
+            let country = countries[i];
+
+            statsDiv.style = "display: none";
+
+            statsDiv.innerHTML = `<div id="statsDiv">
+                    <h3>${country["Country"]}</h3>
+                        <ul>
+                            <li>Number of New confirmed cases : <b>${country["NewConfirmed"]}</b></li>
+                            <li>Total Confirmed cases : <b>${country["TotalConfirmed"]}</b></li>
+                            <li>New Recorded Deaths : <b>${country["NewDeaths"]}</b></li>
+                            <li>Total Number of Deaths : <b>${country["TotalDeaths"]}</b></li>
+                            <li>Number of people who recovered recently : <b>${country["NewRecovered"]}</b></li>
+                            <li>Total number of people who have successfully recovered : <b>${country["TotalRecovered"]}</b> </li>
+                    </ul>
+                    </div>`;
+            break;
+          } else {
+            statsDiv.innerHTML = `<h3>Country not found!</h3>`;
+          }
+        }
+      })
+
+      .catch((error) => console.log("error", error));
+  }
+
+  submitBtn.addEventListener("click", () => {
+    getData();
+  });
+};
+
 let body = document.getElementById("body");
 
-let options = document.getElementById("country-select")
+let options = document.getElementById("country-select");
 
-let statsDiv = document.createElement('div')
-statsDiv.id = 'statsDiv'
+let statsDiv = document.createElement("div");
+statsDiv.id = "statsDiv";
 
+statsDiv.style = "display: none";
+statsDiv.className = "container p-3 mb-2 bg-primary text-white";
 
+body.appendChild(statsDiv);
 
-statsDiv.style = "d-none"
-statsDiv.className = "container p-3 mb-2 bg-primary text-white"
+let submitBtn = document.getElementById("submitBtn");
 
-body.appendChild(statsDiv)
+let selectedCountry = document.getElementById("country-select");
 
-let submitBtn = document.getElementById('submitBtn')
-
-let selectedCountry = document.getElementById('country-select')
-
-let allCountriesNames = []
-
+let allCountriesNames = [];
 
 // this function takes all the countries names and adds it to be used as the autocomplete values
 
-function getNames() {
-    fetch("https://api.covid19api.com/summary")
-        .then(response => response.json())
-        .then(countries => countries['Countries'])
-        .then(countries => countries.forEach(country => allCountriesNames.push(country)))
-        .then(names => {
-            for (let i = 0; i < allCountriesNames.length; i++) {
-                options.innerHTML += ` <option value=${allCountriesNames[i]['Slug']}>${allCountriesNames[i]['Country']}</option>`
-            }
-        })
-
-
-
-}
-
-
-getNames();
-
-
+(function getNames() {
+  fetch("https://api.covid19api.com/summary")
+    .then((response) => response.json())
+    .then((countries) => countries["Countries"])
+    .then((countries) =>
+      countries.forEach((country) => allCountriesNames.push(country))
+    )
+    .then((names) => {
+      for (let i = 0; i < allCountriesNames.length; i++) {
+        options.innerHTML += ` <option value=${allCountriesNames[i]["Slug"]}>${allCountriesNames[i]["Country"]}</option>`;
+      }
+    });
+})();
 
 //this function retrieves the summary data.
 
 function getData() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+  fetch("https://api.covid19api.com/summary", requestOptions)
+    .then((response) => response.json())
+    .then((response) => response["Countries"])
+    .then((countries) => {
+      //let form = document.getElementById('countryname')
+      let data = selectedCountry.value;
 
-    fetch("https://api.covid19api.com/summary", requestOptions)
-        .then(response => response.json())
-        .then(response => response['Countries'])
-        .then(countries => {
+      for (let i = 0; i < countries.length; i++) {
+        if (data.toUpperCase() === countries[i]["Slug"].toUpperCase()) {
+          let country = countries[i];
 
-            //let form = document.getElementById('countryname')
-            let data = selectedCountry.value;
+          statsDiv.style = "d-block";
 
-            for (let i = 0; i < countries.length; i++) {
-                if (data.toUpperCase() === countries[i]['Slug'].toUpperCase()) {
-
-
-                    let country = countries[i]
-
-
-                    console.log("Country Found")
-                    statsDiv.style = "d-block"
-
-
-                    statsDiv.innerHTML = `<div id="statsDiv">
-                    <h3>${ country['Country']}</h3>
+          statsDiv.innerHTML = `<div id="statsDiv">
+                    <h3>${country["Country"]}</h3>
                         <ul>
                             <li>Number of New confirmed cases : <b>${country["NewConfirmed"]}</b></li>
-                            <li>Total Confirmed cases : <b>${country['TotalConfirmed']}</b></li>
+                            <li>Total Confirmed cases : <b>${country["TotalConfirmed"]}</b></li>
                             <li>New Recorded Deaths : <b>${country["NewDeaths"]}</b></li>
-                            <li>Total Number of Deaths : <b>${country['TotalDeaths']}</b></li>
-                            <li>Number of people who recovered recently : <b>${country['NewRecovered']}</b></li>
-                            <li>Total number of people who have successfully recovered : <b>${country['TotalRecovered']}</b> </li>
+                            <li>Total Number of Deaths : <b>${country["TotalDeaths"]}</b></li>
+                            <li>Number of people who recovered recently : <b>${country["NewRecovered"]}</b></li>
+                            <li>Total number of people who have successfully recovered : <b>${country["TotalRecovered"]}</b> </li>
                     </ul>
-                    </div>`
-                    break;
+                    </div>`;
+          break;
+        } else {
+          statsDiv.innerHTML = `<h3>Country not found!</h3>`;
+        }
+      }
+    })
 
-
-                }
-                else {
-                    console.log("country not found")
-                    statsDiv.innerHTML = `<h3>Country not found!</h3>`
-                }
-
-            }
-        })
-
-
-
-
-        .catch(error => console.log('error', error));
-
+    .catch((error) => console.log("error", error));
 }
 
-
 submitBtn.addEventListener("click", () => {
-
-
-
-    console.log("event fired the function getData() !")
-
-
-    getData();
-
-
-
-})
-
-
+  getData();
+});
